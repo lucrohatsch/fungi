2990aaaf86f2ef22db2bf92d03af82e038a2ebd8
luc <lucas.rohatsch@gmail.com>
2021-09-06 17:35:47 -0300

    Demo_Graficos2

--- static/js/graficos.js
+++ static/js/graficos.js
@@ -7,17 +7,13 @@ var ctx = document.getElementById('grafico_temp').getContext("2d");
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['0', '2', '4', '6', '8', '10', '12'],
+        
         datasets: [{
             label: 'Temperatura',
-            data: [25.1, 19, 15, 18, 27, 32, 33],
+            
             borderColor: 'rgb(230, 172, 25)',
             borderWidth: 1},
-            {label: 'Humedad',
-            data: [1, 19, 2, 6, 5, 50, 33],
-            borderWidth: 1,
-            borderColor: 'rgb(25, 104, 230)'
-        }]
+            ]
     },
     
     options: {
