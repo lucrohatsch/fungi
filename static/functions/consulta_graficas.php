f4941c455dabde953b042d9c3aae9871eddb3ead
luc <lucas.rohatsch@gmail.com>
2021-09-27 14:56:50 -0300

    sincro_php

--- static/functions/consulta_graficas.php
+++ static/functions/consulta_graficas.php
@@ -4,7 +4,7 @@ $hoy = date("Y-m-d H:i:s");
 $ayer = date("Y-m-d H:i:s", strtotime('-1 day'));
 
 
-$SQLG="SELECT temperatura.FECHA, temperatura.TEMP, humedad.HUME, co2.CO2 FROM temperatura INNER JOIN humedad ON temperatura.FECHA = humedad.FECHA WHERE temperatura.FECHA BETWEEN '$ayer' AND '$hoy' AND temperatura.LOTE = 'girgola02'";
+$SQLG="SELECT temperatura.FECHA, temperatura.TEMP, humedad.HUME FROM temperatura INNER JOIN humedad ON temperatura.FECHA = humedad.FECHA WHERE temperatura.FECHA BETWEEN '$ayer' AND '$hoy' AND temperatura.LOTE = 'girgola02' AND humedad.LOTE = 'girgola02'";
 
 //printf($SQLG);
 
--- static/functions/verificar_usuario.php
+++ static/functions/verificar_usuario.php
@@ -1,29 +1,32 @@
 <?php  
 
 try{
-	//$USR=htmlentities(addcslashes($_POST['usr']));
-	$USR=$_POST['usr'];
-	//$PASS=htmlentities(addcslashes($_POST['pass']));
-	$PASS=$_POST['pass'];
+        //$USR=htmlentities(addcslashes($_POST['usr']));
+        $USR=$_POST['usr'];
+        //$PASS=htmlentities(addcslashes($_POST['pass']));
+        $PASS=$_POST['pss'];
 
-	$enlace = new mysqli ("localhost", "cultivoiot", "235689ciotDESA","mundo_fungi");
-	
-	$sql="SELECT * FROM `usuarios` WHERE `USR`='$USR' AND `PASS`='$PASS'";
+        $enlace = new mysqli ("localhost", "cultivoiot", "235689ciotDESA","mundo_fungi");
 
-	$verifica=$enlace->query($sql);
-	$num_reg=$verifica->num_rows;
+        $sql="SELECT * FROM `usuarios` WHERE `USR_NAME`='$USR' AND `USR_PASS`='$PASS'";
 
-	if ($num_reg!=0){
-		session_start();
-		$_SESSION["usr_fungi"]=$_POST['usr'];
-		header("location: ../../index.php");
+//      echo $sql;
+        $verifica=$enlace->query($sql);
+//      echo $verifica;
+        $num_reg=$verifica->num_rows;
 
-	}else{
-		header("location:../../login.html");
-	}
+        if ($num_reg!=0){
+                session_start();
+                $_SESSION["usr_fungi"]=$_POST['usr'];
+                header("location: ../../index.php");
+
+        }else{
+                header("location:../../login.html");
+                //echo $num_reg;
+        }
 
 }catch(Exception $e){
-	die("Error: ".$e->getMessage());
+        die("Error: ".$e->getMessage());
 }
 
 ?>
